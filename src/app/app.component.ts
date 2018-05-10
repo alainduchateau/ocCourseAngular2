32df6be1bf9067a5bb4ec2787e7a4353dfc5d055
alainduchateau <alain.duchateau@gmail.com>
2018-05-10 14:05:48 +0200

    start of signIn / Out with firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  constructor(){
+    const config = {
+      apiKey: "AIzaSyAyl85HzHDPYuxWAHEcHYKRNJ7JeCT2zYs",
+      authDomain: "occourseangular2.firebaseapp.com",
+      databaseURL: "https://occourseangular2.firebaseio.com",
+      projectId: "occourseangular2",
+      storageBucket: "occourseangular2.appspot.com",
+      messagingSenderId: "197884554989"
+    };
+    firebase.initializeApp(config);
+  }
 }
