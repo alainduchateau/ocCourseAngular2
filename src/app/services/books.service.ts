ed6b76497a77373400b88c7507afe30bdaa6986e
alainduchateau <alain.duchateau@gmail.com>
2018-05-10 14:58:45 +0200

    Booker Service

--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+import { Book } from '../models/book.model';
+import * as firebase from 'firebase';
 
 @Injectable()
 export class BooksService {
 
-  constructor() { }
+  books: Book[] = [];
+  booksSubject = new Subject<Book[]>();
 
+  emitBooks() {
+    this.booksSubject.next(this.books);
+  }
+
+  saveBooks() {
+    firebase.database().ref('/books').set(this.books);
+  }
 }
