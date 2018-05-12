94c0217c4ce446c5b7bcbb999cee95b4c7cf3489
alainduchateau <alain.duchateau@gmail.com>
2018-05-12 15:46:39 +0200

    gitIgnoreUpdate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,15 @@ import * as firebase from 'firebase';
 })
 export class AppComponent {
   constructor(){
-    
+    const config = {
+      apiKey: "AIzaSyAyl85HzHDPYuxWAHEcHYKRNJ7JeCT2zYs",
+      authDomain: "occourseangular2.firebaseapp.com",
+      databaseURL: "https://occourseangular2.firebaseio.com",
+      projectId: "occourseangular2",
+      storageBucket: "occourseangular2.appspot.com",
+      messagingSenderId: "197884554989"
+    };
+    firebase.initializeApp(config);
   }
     
 }
